f4fc73d24c59310b0779645aea2d90a0f0872512
Plus-Plus <98767612+94025721@users.noreply.github.com>
2022-12-11 13:25:35 +0100

    Alarm Update
    Updated efficiency. Deleted state machine

---
+++ AlarmTest/AlarmTest/AlarmTest.ino
@@ -0,0 +1,8 @@
+void setup() {
+  Serial.begin(9600);
+  AlarmSetup();
+}
+
+void loop() {
+  // alarmBeep();
+}
--- MainRailroadCrossing/MainRailroadCrossing.ino
+++ MainRailroadCrossing/MainRailroadCrossing.ino
@@ -18,10 +18,4 @@ void setup() {
 }
 
 void loop() {
-  display_State_Machine();
-  barrier_State_Machine();
-  button_State_Machine_All_Buttons();
-  traffic_Light_State_Machine();
-  alarm_State_Machine();
-  serial_Loop();
 }
